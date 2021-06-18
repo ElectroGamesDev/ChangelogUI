284312ad5a36963440f75af4303a0a445b02f0ea
Electro Games <34932094+ElectroGamesYT@users.noreply.github.com>
2021-06-18 15:04:45 -0500

    Update Main.php

--- src/Electro/Changelog/Main.php
+++ src/Electro/Changelog/Main.php
@@ -2,6 +2,9 @@
 
 namespace Electro\Changelog;
 
+use jojoe77777\FormAPI\SimpleForm;
+use jojoe77777\FormAPI\CustomForm;
+
 use pocketmine\Server;
 use pocketmine\Player;
 
@@ -17,40 +20,39 @@ use pocketmine\command\CommandSender;
 use pocketmine\command\CommandExecutor;
 use pocketmine\command\ConsoleCommandSender;
 
-use jojoe77777\FormAPI\SimpleForm;
-
 class Main extends PluginBase implements Listener{
-   
-   public function onEnable(){
-       $this->saveDefaultConfig();
-   }
 
-   public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool {
-       switch($cmd->getName()) {
-             case "changelog":
-                 if($sender instanceof Player) {
+    public function onEnable(){
+        $this->saveDefaultConfig();
+    }
+
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool {
+        switch($cmd->getName()) {
+            case "changelog":
+                if($sender instanceof Player) {
                     $this->openChangelog($sender);
-                 }else{
-                        $sender->sendMessage("§cThis command can only be used in-game");
-                        return true;
-                    }
-                 return true;
-       }
-       return true;
-   }
+                }else{
+                    $sender->sendMessage("§cThis command can only be used in-game");
+                    return true;
+                }
+                return true;
+        }
+        return true;
+    }
 
-   public function openChangelog($sender){ 
-       $api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
-       $form = $api->createSimpleForm(function (Player $sender, int $data = null) {
-           $result = $data;
-           if($result === null){
-               return true;
-           }             
-           });
-           $form->setTitle($this->getConfig()->get("title"));
-           $form->setContent($this->getConfig()->get("description"));
-           $form->addButton($this->getConfig()->get("close_button"), 0, "textures/ui/realms_green_check");
-           $form->sendToPlayer($sender);
-           return $form;
-   }
+    public function openChangelog($sender){
+//        $api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
+//        $form = $api->createSimpleForm(function (Player $sender, int $data = null) {
+        $form = new SimpleForm(function (Player $player, int $data = null){
+            $result = $data;
+            if($result === null){
+                return true;
+            }
+        });
+        $form->setTitle($this->getConfig()->get("title"));
+        $form->setContent($this->getConfig()->get("description"));
+        $form->addButton($this->getConfig()->get("close_button"), 0, "textures/ui/realms_green_check");
+        $form->sendToPlayer($sender);
+        return $form;
+    }
 }
